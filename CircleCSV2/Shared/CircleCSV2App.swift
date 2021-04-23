8367ec74e082d681aa8045c5e67387b54c5e7bf2
Sam Foulger <samfoulger@gmail.com>
2021-04-22 18:43:32 -0700

    test

---
+++ CircleCSV2/Shared/CircleCSV2App.swift
@@ -0,0 +1,17 @@
+//
+//  CircleCSV2App.swift
+//  Shared
+//
+//  Created by selfoulger on 4/22/21.
+//
+
+import SwiftUI
+
+@main
+struct CircleCSV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
